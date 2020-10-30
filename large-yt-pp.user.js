a7ab0b18c7da8a9e9eaea84c67bb68411160db16
Besnn <besniknuro74@gmail.com>
2020-10-30 16:36:07 +0100

    Create large-yt-pp.user.js

---
+++ large-yt-pp.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name     Large YT Profile Pics
+// @version  1
+// @match    *://yt3.ggpht.com/*
+// @grant    none
+// ==/UserScript==
+
+var imageURL = window.location.toString();
+var regex = /=.*/;
+
+window.addEventListener('load', function() {
+    window.location = imageURL.replace(regex,"");
+		window.setTimeout(3);
+	}, false);
