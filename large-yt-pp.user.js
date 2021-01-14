0dffd56da95d6b7f4c2c35a5747c14f70e52d8b7
Besnik Nuro <besniknuro74@gmail.com>
2021-01-14 16:19:03 +0100

    Update large-yt-pp.user.js

--- large-yt-pp.user.js
+++ large-yt-pp.user.js
@@ -2,6 +2,8 @@
 // @name     Large YT Profile Pics
 // @author   Besnn
 // @version  1
+// @icon     https://raw.githubusercontent.com/Besnn/enlarge-youtube-profile-pics/main/img/favicon.ico
+// icon courtesy of Freepik
 // @match    *://yt3.ggpht.com/*
 // @grant    none
 // ==/UserScript==
